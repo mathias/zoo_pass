ee264e37243ddc8768c12c5045f9bc0d79868156
Matt Gauger <matt.gauger@gmail.com>
2012-06-20 19:13:48 -0500

    Iniitial version. One hit wonder.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A simple passphrase creator for non-secure things. Use it for creating one-time tokens, sign-up codes, and anything else that doesn't require security.
 
+Over 130 animals of entropy in every passphrase! Safe (and fun) for kids!
+
 ## Installation
 
 Add this line to your application's Gemfile:
--- lib/zoo_pass.rb
+++ lib/zoo_pass.rb
@@ -1,5 +1,10 @@
-require "zoo_pass/version"
+#require "zoo_pass/version"
 
 module ZooPass
-  # Your code goes here...
+  ANIMALS = %w( aardvark alligator alpaca anteater antelope ape armadillo baboon badger bat bear beaver bighorn bison boar buffalo bull bunny burro camel canary capybara cat chameleon cheetah chimpanzee chipmunk civet colt cougar cow coyote crocodile crow deer dingo dog donkey elephant elk ewe fawn ferret finch fish fox frog gazelle giraffe gnu goat gopher gorilla hamster hare hedgehog hippopotamus hog horse hyena ibex iguana impala jackal jaguar kangaroo kid kitten koala lamb lemur leopard lion lizard llama lynx mare marmoset marten mink mole mongoose monkey moose mouse mule musk-ox muskrat mustang newt ocelot opossum orangutan otter ox panda panther parakeet parrot pig platypus pony porcupine porpoise puma puppy rabbit raccoon ram rat reindeer reptile rhino salamander seal sheep shrew skunk sloth snake squirrel stallion steer tiger toad turtle walrus warthog weasel whale wildcat wolf wolverine wombat woodchuck yak zebra )
+
+  def self.generate(length = 3)
+    ANIMALS.shuffle.take(length).join('-')
+  end
+
 end
