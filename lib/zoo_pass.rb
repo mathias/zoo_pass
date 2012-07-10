86741c63264d1decf96cb17b47741b5efdf80576
Matt Gauger <matt.gauger@gmail.com>
2012-07-10 13:58:59 -0500

    Allow developer to specify animal list.
    - Closes #1.

--- lib/zoo_pass.rb
+++ lib/zoo_pass.rb
@@ -1,10 +1,19 @@
-#require "zoo_pass/version"
-
 module ZooPass
-  ANIMALS = %w( aardvark alligator alpaca anteater antelope ape armadillo baboon badger bat bear beaver bighorn bison boar buffalo bull bunny burro camel canary capybara cat chameleon cheetah chimpanzee chipmunk civet colt cougar cow coyote crocodile crow deer dingo dog donkey elephant elk ewe fawn ferret finch fish fox frog gazelle giraffe gnu goat gopher gorilla hamster hare hedgehog hippopotamus hog horse hyena ibex iguana impala jackal jaguar kangaroo kid kitten koala lamb lemur leopard lion lizard llama lynx mare marmoset marten mink mole mongoose monkey moose mouse mule musk-ox muskrat mustang newt ocelot opossum orangutan otter ox panda panther parakeet parrot pig platypus pony porcupine porpoise puma puppy rabbit raccoon ram rat reindeer reptile rhino salamander seal sheep shrew skunk sloth snake squirrel stallion steer tiger toad turtle walrus warthog weasel whale wildcat wolf wolverine wombat woodchuck yak zebra )
+  DEFAULT_LENGTH = 3
+  ANIMALS = %w(aardvark alligator alpaca anteater antelope ape armadillo baboon badger bat bear beaver bighorn bison boar buffalo bull bunny burro camel canary capybara cat chameleon cheetah chimpanzee chipmunk civet colt cougar cow coyote crocodile crow deer dingo dog donkey elephant elk ewe fawn ferret finch fish fox frog gazelle giraffe gnu goat gopher gorilla hamster hare hedgehog hippopotamus hog horse hyena ibex iguana impala jackal jaguar kangaroo kid kitten koala lamb lemur leopard lion lizard llama lynx mare marmoset marten mink mole mongoose monkey moose mouse mule musk-ox muskrat mustang newt ocelot opossum orangutan otter ox panda panther parakeet parrot pig platypus pony porcupine porpoise puma puppy rabbit raccoon ram rat reindeer reptile rhino salamander seal sheep shrew skunk sloth snake squirrel stallion steer tiger toad turtle walrus warthog weasel whale wildcat wolf wolverine wombat woodchuck yak zebra)
 
-  def self.generate(length = 3)
-    ANIMALS.shuffle.take(length).join('-')
+  def self.generate(length = DEFAULT_LENGTH)
+    ZooPass::Generator.new.generate(length)
   end
 
+  class Generator
+    def initialize(animals = ANIMALS)
+      @animals = animals
+    end
+
+    def generate(length = DEFAULT_LENGTH)
+      @animals.shuffle.take(length).join('-')
+    end
+
+  end
 end
--- lib/zoo_pass/version.rb
+++ lib/zoo_pass/version.rb
@@ -1,3 +1,3 @@
 module ZooPass
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
